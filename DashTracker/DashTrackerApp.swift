673bfbcdf672a8c8f42292b5b2525ad0f99d94fb
Anders Bjorland <callmerubb@gmail.com>
2024-03-11 10:12:18 +0100

    Initial Commit

---
+++ DashTracker/DashTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DashTrackerApp.swift
+//  DashTracker
+//
+//  Created by Anders Bjorland on 11/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct DashTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
